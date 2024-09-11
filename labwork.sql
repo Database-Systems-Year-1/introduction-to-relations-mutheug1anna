4302fd722c59eaf85ee9e8eb1468f2d68db27e65
mutheug1anna <167431850+mutheug1anna@users.noreply.github.com>
2024-09-11 11:06:12 +0300

    Add files via upload
    Lab work

---
+++ labwork.sql
@@ -0,0 +1,10 @@
+create table Branch_Supplier (branch_id int,supplier_name varchar(50),supply_type varchar(50));
+insert into Branch_Supplier (branch_id,supplier_name,supply_type)
+values
+(2,'Hammer Mill','Paper'),
+(2,'Uni-ball','Writing Utensils'),
+(3,'Patriot Paper','Paper'),
+(2,'J.T.Forms & Labels','Custom Forms'),
+(3,'Uni-ball','Writing Utensils'),
+(3,'Hammer Mill','Paper'),
+(3,'Stamford Labels','Custom Forms');
